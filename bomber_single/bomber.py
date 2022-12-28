5bf188a845d33d139ed773ed3c0bbe5f994ecd66
converga <converga@yandex.ru>
2022-12-28 23:38:13 +0300

    refactoring, addin gconcurrent.futures instead of Threads

--- bomber_single/bomber.py
+++ bomber_single/bomber.py
@@ -1,108 +1,52 @@
 import requests
 import json
-from threading import Thread
+from concurrent.futures import ThreadPoolExecutor
 
 # Изменяемые параметры:
 
-namespace = 6  # Неймспейс
-ticket_number = 10  # Количество заявок на 1 поток
+namespace = 5  # Неймспейс
+ticket_number = 1  # Количество заявок на 1 поток
+num_threads = 20  # Количество потоков
 
-
-# Запрос на регистрацию
+# Запрос
 url = f'https://subzero-billing-{namespace}.testms-test.lognex.ru/api/clinton/1.0/ticket'
 headers = {'Content-Type': 'application/json'}
 
 
 # Функция для бомбардировки запросами в clinton, ничего менять не нужно
 def bomber():
+    print('bomber start')
+
     counter1 = 0
     counter2 = 0
-    with open('json1.json', 'r') as json1:
-        sub_data = json.load(json1)
-        for i in range(ticket_number):
-            counter1 += 1
-            response = requests.post(url, data=json.dumps(sub_data), headers=headers)
+
+    with requests.Session() as s:
+        with open('json1.json', 'r') as json1:
+            sub_data = json.load(json1)
+            for i in range(ticket_number):
+                counter1 += 1
+                with s.post(url, data=json.dumps(sub_data), headers=headers):
+                    print('request 1 done', counter1)
             #return response
 
-    with open('json2.json', 'r') as json2:
-        sub_data = json.load(json2)
-        for j in range(ticket_number):
-            counter2 += 1
-            response_2 = requests.post(url, data=json.dumps(sub_data), headers=headers)
+    print('bomber midway')
+
+    with requests.Session() as s:
+        with open('json2.json', 'r') as json2:
+            sub_data = json.load(json2)
+            for j in range(ticket_number):
+                counter2 += 1
+                with s.post(url, data=json.dumps(sub_data), headers=headers):
+                    print('request 2 done', counter2)
             #return response_2
 
     print('------------------------------------------------')
     print('итого, создано заявок:', counter1+counter2)
 
 
-# Потоки
-thread1 = Thread(target=bomber)
-thread2 = Thread(target=bomber)
-thread3 = Thread(target=bomber)
-thread4 = Thread(target=bomber)
-thread5 = Thread(target=bomber)
-thread6 = Thread(target=bomber)
-thread7 = Thread(target=bomber)
-thread8 = Thread(target=bomber)
-thread9 = Thread(target=bomber)
-thread10 = Thread(target=bomber)
-thread11 = Thread(target=bomber)
-thread12 = Thread(target=bomber)
-thread13 = Thread(target=bomber)
-thread14 = Thread(target=bomber)
-thread15 = Thread(target=bomber)
-thread16 = Thread(target=bomber)
-thread17 = Thread(target=bomber)
-thread18 = Thread(target=bomber)
-thread19 = Thread(target=bomber)
-thread20 = Thread(target=bomber)
+# Создание пула потоков
+with ThreadPoolExecutor(max_workers=num_threads) as executor:
+    # Запуск потоков
+    results = [executor.submit(bomber) for _ in range(num_threads)]
 
-
-def start_threads():
-    thread1.start()
-    thread2.start()
-    thread3.start()
-    thread4.start()
-    thread5.start()
-    thread6.start()
-    thread7.start()
-    thread8.start()
-    thread9.start()
-    thread10.start()
-    thread11.start()
-    thread12.start()
-    thread13.start()
-    thread14.start()
-    thread15.start()
-    thread16.start()
-    thread17.start()
-    thread18.start()
-    thread19.start()
-    thread20.start()
-
-
-def stop_threads():
-    thread1.join()
-    thread2.join()
-    thread3.join()
-    thread4.join()
-    thread5.join()
-    thread6.join()
-    thread7.join()
-    thread8.join()
-    thread9.join()
-    thread10.join()
-    thread11.join()
-    thread12.join()
-    thread13.join()
-    thread14.join()
-    thread15.join()
-    thread16.join()
-    thread17.join()
-    thread18.join()
-    thread19.join()
-    thread20.join()
-
-
-start_threads()
-stop_threads()
+print('All threads have completed')
