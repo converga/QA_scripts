e420c0140267a0989043010a1a4a3ca49b20051e
Артем Дроздецкий <adrozdetskiy@moysklad.ru>
2023-06-18 15:32:12 +0300

    fix in progress

---
+++ creating_products2.py
@@ -0,0 +1,70 @@
+import requests
+import json
+import uuid
+import argparse
+
+# Command line arguments
+parser = argparse.ArgumentParser(description='Product registration script.')
+parser.add_argument('--counter', type=int, help='Product number alias')
+parser.add_argument('--namespace', type=str, help='Namespace')
+parser.add_argument('--product_number', type=int, help='Number of products to create')
+parser.add_argument('--product_name', type=str, help='Name of the product')
+args = parser.parse_args()
+
+# set up url for creating the product
+url = f'https://admin-{args.namespace}.testms-test.lognex.ru/api/tarifflego/1.0/product'
+headers = {'Content-Type': 'application/json'}
+
+# set data to activating request
+data_activate = {
+    "status": "ACTIVE"
+}
+json_activate = json.dumps(data_activate)
+
+free_id = None
+trial_id = None
+for product in range(args.product_number):
+    with open('bomber\product_sample.json', encoding='utf-8') as product_data:
+        sub_data = json.load(product_data)
+        sub_data['name'] = args.product_name + f'{args.counter}'
+
+    for tariff in sub_data["tariffs"]:
+        if tariff["name"]["en"] == "Free":
+            free_id = str(uuid.uuid4())
+            tariff["id"] = free_id
+        elif tariff["name"]["en"] == "Trial":
+            trial_id = str(uuid.uuid4())
+            tariff["id"] = trial_id
+        else:
+            tariff["id"] = str(uuid.uuid4())
+
+    # Assigning Free and Trial to a product
+    sub_data['trial']['tariffId'] = trial_id
+    sub_data['free']['tariffId'] = free_id
+
+    # convert the modified data back to json encoded to utf-8 and with no ascii
+    json_data = json.dumps(sub_data, ensure_ascii=False, indent=4).encode('utf-8')
+
+    # make a POST request to the specified URL with the json data
+    response = requests.post(url, json_data, headers=headers)
+
+    # check the status code of the response
+    if response.ok:
+        print('Success')
+        response_data = response.json()
+        product_id = response_data.get("id")
+        url2 = f'https://admin-{args.namespace}.testms-test.lognex.ru/api/tarifflego/1.0/product/{product_id}/version/1/status'
+
+        response_activate = requests.put(url2, json_activate, headers=headers)
+        if response_activate.status_code == 204:
+            print(f'Product{args.counter} successfully activated!')
+            print('------------------------------------------')
+            args.counter += 1
+        else:
+            print(f'Request failed with status code {response_activate.status_code}')
+
+    else:
+        print(f'Failed, {response}')
+
+# to run the script: 
+# python creating_products2.py --counter 1 --namespace billing-1 --product_number 3 --product_name "test_product"  
